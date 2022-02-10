d864d54c9f3f5207ba4a30ef2f0497652de826b5
jferrergil <jferrergil1@gmail.com>
2022-02-10 22:51:25 +0100

    changes in login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,40 +1,61 @@
-import "./Login.scss";
+import { FormEvent, useState } from 'react';
+import './Login.scss';
 
 export default function Login() {
+  const [isLoading, setIsLoading] = useState(false);
 
   const handeleForm = (event: FormEvent) => {
-    event.preventDefault()
-  }
+    setIsLoading(true);
+    event.preventDefault();
+    setTimeout(() => {
+      setIsLoading(false);
+      alert('Login Successful');
+    }, 2000);
+  };
 
-  return (
-    <div className="center">
-      <h1>Login</h1>
-      <form onSubmit={(e) => handeleForm(e)}>
-        <div className="txt_field">
-          <input
-            type="text"
-            className="form-control"
-            name="user-email"
-            id="user-email"
-            required
-          />
-          <label htmlFor="user-email">Email</label>
-        </div>
-        <div className="txt_field">
-          <input
-            type="password"
-            className="form-control"
-            name="user-passwd"
-            id="user-passwd"
-            required
-          />
-          <label htmlFor="user-passwd">Password</label>
-        </div>
-        <button className="button" type="submit">
-          Login
-        </button>
-        <div className="signup_link"></div>
-      </form>
-    </div>
-  );
+  // eslint-disable-next-line no-lone-blocks
+  {
+    return isLoading ? (
+      <div className='lds-roller'>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    ) : (
+      <div className='center'>
+        <h1>Login</h1>
+        <form onSubmit={(e) => handeleForm(e)}>
+          <div className='txt_field'>
+            <input
+              type='text'
+              className='form-control'
+              name='user-email'
+              id='user-email'
+              required
+            />
+            <label htmlFor='user-email'>Email</label>
+          </div>
+          <div className='txt_field'>
+            <input
+              type='password'
+              className='form-control'
+              name='user-passwd'
+              id='user-passwd'
+              required
+            />
+            <label htmlFor='user-passwd'>Password</label>
+          </div>
+          <button className='button' type='submit'>
+            Login
+          </button>
+          <div className='signup_link'></div>
+        </form>
+      </div>
+    );
+  }
 }
